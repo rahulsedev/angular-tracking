4adbad0984b519d7197ea197144c7d9e4cc3a1ca
rahulsedev <rahulsedev@gmail.com>
2019-10-30 09:47:37 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TrackclickComponent } from './trackclick/trackclick.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,
+      RouterModule.forRoot([
+      { path: '', component: TrackclickComponent }
+    ])
+   ],
+  declarations: [ AppComponent, HelloComponent, TrackclickComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/trackclick/trackclick.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trackclick',
+  templateUrl: './trackclick.component.html',
+  styleUrls: ['./trackclick.component.css']
+})
+export class TrackclickComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
